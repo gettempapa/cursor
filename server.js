5f5cfc89c19233d65ff1e6f373761c06eddfd123
gettempapa <julianlohnes@gmail.com>
2025-03-01 18:24:18 -0800

    Switch from CDN to installed Three.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 8080;
 // Serve static files from the public directory
 app.use(express.static('public'));
 
+// Serve node_modules (for Three.js)
+app.use('/node_modules', express.static('node_modules'));
+
 // Serve the game at the root URL
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
