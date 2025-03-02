e4daf8a732a2832bedbda93d5b3e071796e29d03
gettempapa <julianlohnes@gmail.com>
2025-03-01 20:54:02 -0800

    Update Three.js imports to use CDN for DigitalOcean deployment

--- public/js/modules/Player.js
+++ public/js/modules/Player.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.157.0/build/three.module.js';
 import { WeaponSystem } from './WeaponSystem.js';
 
 export class Player {
